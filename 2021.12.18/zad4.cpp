516d05deee67e071bfb7f7716642bb40fe9c8656
s24399-pj <s24399@pjwstk.edu.pl>
2022-01-23 19:24:50 +0100

    Poprawka zad4.cpp

--- 2021.12.18/zad4.cpp
+++ 2021.12.18/zad4.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
-
 using namespace std;
-
 double fun(double r){
-    double wynik = 0;
-    for (int i =0;i<r;i++){
-        wynik += i*i;
+    double wynik=0;
+    for (int i=0;i<r;i++){
+        wynik+=i*i;
     }
     return wynik;
 }
 
-auto fSum(double (*f)(double) , int n, int m)-> double{
+auto fSum(double (*f)(double),int n,int m)-> double{
     int suma=0;
     f=&fun;
-    for (int i=n;i<m;i++){
-        suma += fun(m);
-        cout << suma << "\n";
+    for(int i=n;i<m;i++){
+        suma+=fun(m);
+        cout<<suma<<"\n";
     }
     return suma;
 }
@@ -23,6 +21,6 @@ auto fSum(double (*f)(double) , int n, int m)-> double{
 int main(){
     double x=5;
     double y=10;
-    cout << "Suma wynikow = " << fSum(fun,x,y) << "\n";
+    cout << "Suma wynikow = "<< fSum(fun,x,y)<<"\n";
     return 0;
 }
